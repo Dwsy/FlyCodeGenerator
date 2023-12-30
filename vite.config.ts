89a43aa73f7428b0ed12697a5753a8388a398bd2
Dwsy <dwsycode@gmail.com>
2023-12-31 00:26:34 +0800

    use:pgsql-ast-parser add: autoAsSqlCompletion

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import jsx from '@vitejs/plugin-vue-jsx';
-import monkey, { cdn } from 'vite-plugin-monkey';
-import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import jsx from '@vitejs/plugin-vue-jsx'
+import monkey, { cdn } from 'vite-plugin-monkey'
+import AutoImport from 'unplugin-auto-import/vite'
 
-import Components from "unplugin-vue-components/vite";
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    // minify: "esbuild"
-    minify: false
+    minify: 'esbuild'
+    // minify: false
   },
   plugins: [
     jsx({}),
     AutoImport({
-      imports: ["vue", "pinia", "@vueuse/core"],//这里是自动引入api的项目
-      dts: "./src/auto-imports.d.ts",//在这创建.d.ts文件
+      imports: ['vue', 'pinia', '@vueuse/core'], //这里是自动引入api的项目
+      dts: './src/auto-imports.d.ts' //在这创建.d.ts文件
     }),
     vue(),
     monkey({
@@ -26,25 +26,26 @@ export default defineConfig({
         icon: 'http://flydoc.dwsy.link/xwlogoblue.png',
         namespace: 'dwsy/flycodegenerator',
         match: ['http://xwide.dwsy.link/*'],
-        "run-at": 'document-start',
-        updateURL: "https://ghproxy.com/?q=https%3A%2F%2Fraw.githubusercontent.com%2FDwsy%2FFlyCodeGenerator%2Fmain%2Fflycodegenerator.user.js",
-        downloadURL: "https://ghproxy.com/?q=https%3A%2F%2Fraw.githubusercontent.com%2FDwsy%2FFlyCodeGenerator%2Fmain%2Fflycodegenerator.user.js",
-        version: "2.0.0",
-        author: "Dwsy",
-        description: "FlyCodeGenerator",
+        'run-at': 'document-start',
+        updateURL:
+          'https://ghproxy.com/?q=https%3A%2F%2Fraw.githubusercontent.com%2FDwsy%2FFlyCodeGenerator%2Fmain%2Fflycodegenerator.user.js',
+        downloadURL:
+          'https://ghproxy.com/?q=https%3A%2F%2Fraw.githubusercontent.com%2FDwsy%2FFlyCodeGenerator%2Fmain%2Fflycodegenerator.user.js',
+        version: '2.0.0',
+        author: 'Dwsy',
+        description: 'FlyCodeGenerator',
         grant: ['GM_registerMenuCommand']
       },
       build: {
         externalGlobals: {
           // vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
-        },
-      },
-
+        }
+      }
     }),
     Components({
-      dts: "./src/components.d.ts",//创建ts文件
-      extensions: ['vue'],//指定文件的后缀
-      dirs: ["src/components"],//指定路径，自动导入自定义组件
-    }),
-  ],
-});
+      dts: './src/components.d.ts', //创建ts文件
+      extensions: ['vue'], //指定文件的后缀
+      dirs: ['src/components'] //指定路径，自动导入自定义组件
+    })
+  ]
+})
